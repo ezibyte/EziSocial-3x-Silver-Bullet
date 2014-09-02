3d4027d22e091397eb2f73870e51329481a1d59b
Paras Mendiratta <paras@ezibyte.com>
2014-09-02 10:42:43 +0530

    Added Bare minimum Cocos2d for iOS  project

---
+++ examples/Cocos2d/HelloWorld-Cocos2d-ios/HelloWorld-Cocos2d-ios/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  HelloWorld-Cocos2d-ios
+//
+//  Created by Paras Mendiratta on 02/09/14.
+//  Copyright EziByte Technologies 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ examples/Cocos2d/HelloWorld-Cocos2d-ios/HelloWorld-Cocos2d-ios/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld-Cocos2d-ios
+//
+//  Created by Paras Mendiratta on 02/09/14.
+//  Copyright EziByte Technologies 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
